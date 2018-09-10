d68ccaddaf5c359920b9f2b3c55d11cc81060b23
firdayanadeby <43123859+firdayanadeby@users.noreply.github.com>
2018-09-10 21:05:32 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,53 @@
-<?php
+<form action="TugasRekursif.php" method="POST">
+            Masukkan Angka : <input type="text" name="a" ><br>
+                <input type="submit" name="submit" value="Hasil">
+        </form>
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
+<?php
+if (isset($_POST ["submit"])) {
+
+		$a = $_POST ["a"];
+		bawah($a);
+		
+
+	echo "<br>";
+	echo "- - - - - - - - - - - - - - - - - - - - - - - - - - -";
+	echo "<br>";
 }
-rekursif(5);
+function bawah ($a){
+	if ($a >= 1) {
+		samping($a);
+		echo "<br>";
+		$a--;
+		bawah ($a);
+	}
+}
+function samping ($bawah,$samping =1){
+	echo "*";
+	$samping++;
+	if ($samping <=$bawah)
+		samping ($bawah,$samping);
+	
+}
+if (isset($_POST ["submit"])) {
+	$a = $_POST["a"]; //10
+	bawah2(1,$a);
+}
+
+function bawah2($permulaan, $tepi){
+	samping2(1, $permulaan);
+	echo "<br>";
+	$permulaan++;
+	if($permulaan <= $tepi)	
+		bawah2($permulaan, $tepi);
+}
+function samping2($permulaan, $tepi){
+	echo $permulaan;
+	$permulaan++;
+	if($permulaan <= $tepi)
+		samping2($permulaan,$tepi);
+}
+
+
+
 ?>
